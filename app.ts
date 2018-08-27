aa06000476ba39056e6e1e8fea18523ae82b2b78
xjx <644233452@qq.com>
2018-08-27 17:53:22 +0800

    add mqtt code

--- app.ts
+++ app.ts
@@ -7,19 +7,28 @@ module.exports = app => {
         console.log("websocket start");
 
         client.on('connect', () => {
-            client.subscribe('xjx1');
-            client.publish('xjx2', 'xjx2 hello mqtt');
+            client.subscribe('draw/drawClient');
+            client.publish('draw/drawServer', 'hello mqtt, my is Server');
+        })
+
+        client.on('message', function (topic: string, message: string, packet: any) {
+            if (topic.indexOf('draw/drawClient') > -1) {
+                // 处理开锁信息，关闭连接
+                console.log("message: ", message);
+                console.log("packet: ", packet);
+                client.end();
+            }
         })
     });
-    app.on('error', () => {
-        // report error
-    });
-    app.on('request', () => {
-        // log receive request
-    });
-    app.on('response', () => {
-        // ctx.starttime is set by framework
-        // const used = Date.now() - ctx.starttime;
-        // log total cost
-    });
+app.on('error', () => {
+    // report error
+});
+app.on('request', () => {
+    // log receive request
+});
+app.on('response', () => {
+    // ctx.starttime is set by framework
+    // const used = Date.now() - ctx.starttime;
+    // log total cost
+});
 };
